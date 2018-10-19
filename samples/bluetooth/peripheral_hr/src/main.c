db43c57b396060d6da66a015f94add555dae3723
Thea Aldrich <aldrich.thea@gmail.com>
2018-10-18 23:56:12 -0500

    Initial attempt at reading from the sensor over bluetooth
    Signed-off-by: Thea Aldrich <aldrich.thea@gmail.com>

--- samples/bluetooth/peripheral_hr/src/main.c
+++ samples/bluetooth/peripheral_hr/src/main.c
@@ -7,6 +7,8 @@
  */
 
 #include <zephyr/types.h>
+#include <device.h>
+#include <sensor.h>
 #include <stddef.h>
 #include <string.h>
 #include <errno.h>
@@ -20,9 +22,69 @@
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h>
 
-#include <gatt/hrs.h>
-#include <gatt/dis.h>
-#include <gatt/bas.h>
+
+static u8_t temperature = 0;		//This variable holds the current measured temperature
+static struct sensor_value temperature_data;
+/*
+ * SUMMARY: Uses helper function to read the current measured temperature into a response buffer when requested by
+ * 	the other side of the bluetooth connection.
+ */
+static ssize_t read_temp(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+						 void *buf, u16_t len, u16_t offset)
+{
+	return bt_gatt_attr_read(conn,                 // Handed to us by the bluetooth stack
+			                 attr,                 // Handed to us by the bluetooth stack
+			                 buf,                  // Handed to us by the bluetooth stack
+			                 len,                  // Handed to us by the bluetooth stack
+			                 offset,               // Handed to us by the bluetooth stack
+			                 &temperature,         // Value to be pushed into buffer to send to requestor (other side of connection)
+							 sizeof(temperature)   // Size of value to be pushed
+							 );
+}
+
+
+/* SUMMARY: This struct defines a Generic Attribute Profile (GATT).
+ * 	A GATT Profile *HAS* a Service.
+ * 	A Service *HAS* a characteristic.
+ * 	A Characteristic is a piece of data.
+ * SIMPLE Temp Service Declaration
+*/
+static struct bt_gatt_attr attrs[] = {
+		// Environmental Sensing Service bitmask defined in zephyr/include/bluetooth/uuid.h
+		BT_GATT_PRIMARY_SERVICE(BT_UUID_ESS),
+		BT_GATT_CHARACTERISTIC(BT_UUID_TEMPERATURE, //UUID defining what kind of characteristic (data) this is
+				               BT_GATT_CHRC_NOTIFY | BT_GATT_CHRC_READ, // Available Actions (read,write,notify,etc)
+							   BT_GATT_PERM_READ,                               // Available Permissions
+				               read_temp,                                    // Read callback function
+				               NULL,                                    // Write callback function
+				               &temperature								// Value this characteristic represents
+				               )
+};
+
+static struct bt_gatt_service measure_temperature_service = BT_GATT_SERVICE(attrs);
+
+
+/*
+ * SUMMARY: Reads the current temperature and notifies all clients of a new value if they have registered with
+ * 	notify option.
+ */
+void temperature_measurement_notify(void)
+{
+	static u8_t temp_data[2]; 	// [0] = Type
+								// [1] = data primitive (the actual temperature)
+
+	// Poll the temperature sensor
+
+
+	// Pack the data
+	temp_data[0] = 0x06; /* uint8, sensor contact */
+	temp_data[1] = temperature;
+
+	// Notify all clients/peers of a new value
+	bt_gatt_notify(NULL, &attrs[1], &temp_data, sizeof(temp_data));
+}
+
+
 
 struct bt_conn *default_conn;
 
@@ -65,9 +127,8 @@ static void bt_ready(int err)
 
 	printk("Bluetooth initialized\n");
 
-	hrs_init(0x01);
-	bas_init();
-	dis_init(CONFIG_SOC, "Manufacturer");
+	// Register temperature service
+	bt_gatt_service_register(&measure_temperature_service);
 
 	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
@@ -91,8 +152,45 @@ static struct bt_conn_auth_cb auth_cb_display = {
 	.cancel = auth_cancel,
 };
 
+/*
+ * TEMPERATURE SENSOR CONFIGURATION SECTION
+ */
+#define SENSOR_CHAN_TEMP 14 //Which pin sensor is connected to (found via trial and error)
+struct channel_info {
+	int chan;
+	char *dev_name;
+};
+
+static struct channel_info temperature_sensor_config[] = {
+		{SENSOR_CHAN_TEMP, "HDC1008"}
+};
+
 void main(void)
 {
+	/*
+	 * INTIALIZE THE TEMPERATURE SENSOR
+	 */
+	// Sensor array, one element per sensor being used.
+	struct device *sensor_device_array[ARRAY_SIZE(temperature_sensor_config)];
+
+	// Struct to hold measurement from sensor
+	struct sensor_value temperature_data;
+
+	// Bind sensor to device stuct
+	sensor_device_array[0] = device_get_binding(temperature_sensor_config[0].dev_name);
+
+	// If initialized correctly this should NOT be null
+	if (sensor_device_array[0] == NULL) {
+		printk("ERROR: Sensor %s failed to initialize.\n", temperature_sensor_config[0].dev_name);
+	}
+
+	// Populate the first sensor value
+	sensor_channel_get(sensor_device_array[0], temperature_sensor_config[0].chan, &temperature);
+    printk("SANITY CHECK: First temperature sensor reading: %d\n", )
+
+	/*
+	 * INITIALIZE THE BLUETOOTH SENSOR
+	 */
 	int err;
 
 	err = bt_enable(bt_ready);
@@ -108,12 +206,9 @@ void main(void)
 	 * of starting delayed work so we do it here
 	 */
 	while (1) {
-		k_sleep(MSEC_PER_SEC);
+		k_sleep(1000);
 
-		/* Heartrate measurements simulation */
-		hrs_notify();
-
-		/* Battery level simulation */
-		bas_notify();
+		/* Temperature measurement */
+		temperature_measurement_notify();
 	}
 }
